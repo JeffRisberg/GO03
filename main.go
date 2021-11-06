3512a7b9e77f6c7487aa4f4b759e9933c2f9c65b
JeffRisberg <j.s.risberg@gmail.com>
2021-11-06 15:51:57 -0700

    improved example of using a template

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
@@ -12,9 +13,16 @@ func donors(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Here are your donors"))
 }
 
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl.html")
+	daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+	t.Execute(w, daysOfWeek)
+}
+
 func main() {
 	http.HandleFunc("/charities", charities)
 	http.HandleFunc("/donors", donors)
+	http.HandleFunc("/process", process)
 
 	http.Handle("/", http.FileServer(http.Dir("./src")))
 
