77bc06221868c90c88822db793f17902de78f39e
JeffRisberg <j.s.risberg@gmail.com>
2021-11-14 22:05:23 -0800

    added looping example

--- main.go
+++ main.go
@@ -31,11 +31,29 @@ func process2(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, data)
 }
 
+type Donation struct {
+	Charity string
+	Amount  int
+}
+
+type DonationReport struct {
+	Donations []Donation
+}
+
+func process3(w http.ResponseWriter, r *http.Request) {
+	donation1 := &Donation{"Red Cross", 400}
+	donation2 := &Donation{"Stanford University", 500}
+	data := &DonationReport{[]Donation{*donation1, *donation2}}
+	t, _ := template.ParseFiles("tmpl3.html")
+	t.Execute(w, data)
+}
+
 func main() {
 	http.HandleFunc("/charities", charities)
 	http.HandleFunc("/donors", donors)
 	http.HandleFunc("/process", process)
 	http.HandleFunc("/process2", process2)
+	http.HandleFunc("/process3", process3)
 
 	http.Handle("/", http.FileServer(http.Dir("./src")))
 
